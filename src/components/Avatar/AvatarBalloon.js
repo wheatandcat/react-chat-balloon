db171efc401addd94403f62c374fa605126b14d7
“wheatandcat” <wheatandcat@gmail.com>
2018-01-08 00:03:54 +0900

    Fix

--- src/components/Avatar/AvatarBalloon.js
+++ src/components/Avatar/AvatarBalloon.js
@@ -4,22 +4,27 @@ import styled from "styled-components"
 import AvatarBalloon, { Right, Left } from "./"
 
 type Props = {
-  children?: Node,
+  avatarStyle?: CSSStyleDeclaration,
   avatarUtl: string,
+  children?: Node,
   position?: "left" | "right",
 }
 
-export default ({ children, position, avatarUtl }: Props) => {
+export default ({ children, position, avatarUtl, avatarStyle }: Props) => {
   if (position === "right") {
     return (
-      <Right avatarUtl={avatarUtl} position={position}>
+      <Right
+        avatarUtl={avatarUtl}
+        position={position}
+        avatarStyle={avatarStyle}
+      >
         {children}
       </Right>
     )
   }
 
   return (
-    <Left avatarUtl={avatarUtl} position={position}>
+    <Left avatarUtl={avatarUtl} position={position} avatarStyle={avatarStyle}>
       {children}
     </Left>
   )
